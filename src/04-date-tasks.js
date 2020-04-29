6d435542ae50684032bc9404f8d1c1fcdfc9e20d
NexGenUA <mr.rinus@gmail.com>
2020-04-29 09:17:03 +0300

    feat: date complete

--- src/04-date-tasks.js
+++ src/04-date-tasks.js
@@ -19,8 +19,8 @@
  *    'Tue, 26 Jan 2016 13:48:02 GMT' => Date()
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
-function parseDataFromRfc2822(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromRfc2822(d) {
+  return new Date(d);
 }
 
 /**
@@ -34,8 +34,8 @@ function parseDataFromRfc2822(/* value */) {
  *    '2016-01-19T16:07:37+00:00'    => Date()
  *    '2016-01-19T08:07:37Z' => Date()
  */
-function parseDataFromIso8601(/* value */) {
-  throw new Error('Not implemented');
+function parseDataFromIso8601(d) {
+  return +new Date(d);
 }
 
 
@@ -53,8 +53,9 @@ function parseDataFromIso8601(/* value */) {
  *    Date(2012,1,1)    => true
  *    Date(2015,1,1)    => false
  */
-function isLeapYear(/* date */) {
-  throw new Error('Not implemented');
+function isLeapYear(d) {
+  const y = d.getFullYear();
+  return (y % 100 === 0) ? (y % 400 === 0) : (y % 4 === 0);
 }
 
 
@@ -73,8 +74,8 @@ function isLeapYear(/* date */) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,10,0,0,250)     => "00:00:00.250"
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
-function timeSpanToString(/* startDate, endDate */) {
-  throw new Error('Not implemented');
+function timeSpanToString(x, y) {
+  return new Date(y - x).toISOString().slice(11, 23);
 }
 
 
@@ -94,10 +95,16 @@ function timeSpanToString(/* startDate, endDate */) {
  *    Date.UTC(2016,3,5,18, 0) => Math.PI
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
-function angleBetweenClockHands(/* date */) {
-  throw new Error('Not implemented');
+function angleBetweenClockHands(d) {
+  const cd = new Date(d);
+  let h = cd.getUTCHours();
+  h = (h > 12) ? h - 12 : h;
+  const m = cd.getUTCMinutes();
+  let deg = ((h * 30) + (m * 0.5)) - (m * 6);
+  deg = (deg > 180) ? deg - 180 : deg;
+  deg = (deg < 0) ? -deg : deg;
+  return (deg * (Math.PI / 180));
 }
-
 
 module.exports = {
   parseDataFromRfc2822,
